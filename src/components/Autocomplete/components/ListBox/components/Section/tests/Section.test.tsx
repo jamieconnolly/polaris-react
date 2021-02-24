Shopify/polaris

Rebuild AutoComplete internals to fix accessibility issues

src/components/Autocomplete/components/ListBox/components/Section/tests/Section.test.tsx
@@ -1,0 +1,67 @@
+import React from 'react';
+import {mount} from 'tests/modern';
+
+import {Section} from '../Section';
+import {SectionContext} from '../context';
+import {listBoxSectionDataSelector} from '../selectors';
+
+describe('Section', () => {
+  describe('context', () => {
+    it('renders SectionContextProvider with an id', () => {
+      const section = mount(<Section title="title" />);
+
+      expect(section).toContainReactComponent(SectionContext.Provider, {
+        value: expect.any(String),
+      });
+    });
+  });
+
+  describe('list item', () => {
+    it('renders a list item with role=presentation', () => {
+      const section = mount(<Section title="title" />);
+
+      expect(section).toContainReactComponent('li', {
+        role: 'presentation',
+      });
+    });
+
+    it('renders a list item with list box section data selector', () => {
+      const section = mount(<Section title="title" />);
+
+      expect(section).toContainReactComponent(
+        'li' as any,
+        listBoxSectionDataSelector.props,
+      );
+    });
+  });
+
+  describe('list', () => {
+    it('renders an unordered list with role="group', () => {
+      const section = mount(<Section title="title" />);
+
+      expect(section).toContainReactComponent('ul', {
+        role: 'group',
+      });
+    });
+
+    it('renders an unordered list with aria labelledby matching the context value', () => {
+      const section = mount(<Section title="title" />);
+
+      const contextValue = section.find(SectionContext.Provider)!.prop('value');
+      const ariaLabelledByValue = section.find('ul')!.prop('aria-labelledby');
+
+      expect(ariaLabelledByValue).toBe(contextValue);
+    });
+  });
+
+  it('renders children', () => {
+    const Child = () => <li>Child</li>;
+    const section = mount(
+      <Section title="title">
+        <Child />
+      </Section>,
+    );
+
+    expect(section).toContainReactComponent(Child);
+  });
+});

jamieconnolly/polaris-react

 - Replace internals with new combobox and list box
 - Add support for legacy combobox
 - Allow TextField and Checkbox to leverage new combobox
 - Include additional test coverage for autocomplete and new components
Co-authored-by: Andrew Musgrave <andrew.musgrave@shopify.com>
Co-authored-by: Daniel Leroux <daniel.leroux@shopify.com>

src/components/Autocomplete/components/ListBox/components/Section/tests/Section.test.tsx
@@ -1,0 +1,67 @@
+import React from 'react';
+import {mountWithApp} from 'test-utilities';
+
+import {Section} from '../Section';
+import {SectionContext} from '../context';
+import {listBoxSectionDataSelector} from '../selectors';
+
+describe('Section', () => {
+  describe('context', () => {
+    it('renders SectionContextProvider with an id', () => {
+      const section = mountWithApp(<Section title="title" />);
+
+      expect(section).toContainReactComponent(SectionContext.Provider, {
+        value: expect.any(String),
+      });
+    });
+  });
+
+  describe('list item', () => {
+    it('renders a list item with role=presentation', () => {
+      const section = mountWithApp(<Section title="title" />);
+
+      expect(section).toContainReactComponent('li', {
+        role: 'presentation',
+      });
+    });
+
+    it('renders a list item with list box section data selector', () => {
+      const section = mountWithApp(<Section title="title" />);
+
+      expect(section).toContainReactComponent(
+        'li' as any,
+        listBoxSectionDataSelector.props,
+      );
+    });
+  });
+
+  describe('list', () => {
+    it('renders an unordered list with role="group', () => {
+      const section = mountWithApp(<Section title="title" />);
+
+      expect(section).toContainReactComponent('ul', {
+        role: 'group',
+      });
+    });
+
+    it('renders an unordered list with aria labelledby matching the context value', () => {
+      const section = mountWithApp(<Section title="title" />);
+
+      const contextValue = section.find(SectionContext.Provider)!.prop('value');
+      const ariaLabelledByValue = section.find('ul')!.prop('aria-labelledby');
+
+      expect(ariaLabelledByValue).toBe(contextValue);
+    });
+  });
+
+  it('renders children', () => {
+    const Child = () => <li>Child</li>;
+    const section = mountWithApp(
+      <Section title="title">
+        <Child />
+      </Section>,
+    );
+
+    expect(section).toContainReactComponent(Child);
+  });
+});